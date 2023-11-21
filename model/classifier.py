029cea66449b502b2a4d324608f59da28f5a3b00
jules20022000 <jules.maglione@epfl.ch>
2023-11-21 11:01:15 +0100

    embeddings classifier with BERT

--- model/classifier.py
+++ model/classifier.py
@@ -3,16 +3,14 @@ import torch.nn as nn
 
 
 class Classifier(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
+    def __init__(self, input_size, num_classes, hidden_size=64):
         super(Classifier, self).__init__()
         self.fc1 = nn.Linear(input_size, hidden_size)
         self.relu = nn.ReLU()
         self.fc2 = nn.Linear(hidden_size,num_classes)
-        self.softmax = nn.Softmax(dim=1)
   
     def forward(self, x):	
         out = self.fc1(x)
         out = self.relu(out)
         out = self.fc2(out)
-        out = self.softmax(out)
         return out
